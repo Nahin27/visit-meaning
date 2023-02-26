1adcc6b9333e387485872676559aede94eb603bd
Nahin27 <nahin271199@gmail.com>
2023-02-26 19:06:43 +0600

    working on local computer

---
+++ app.js
@@ -0,0 +1,32 @@
+import { serve } from "https://deno.land/std@0.171.0/http/server.ts";
+import { configure, renderFile } from "https://deno.land/x/eta@v2.0.0/mod.ts";
+
+configure({
+    views: `${Deno.cwd()}/views`
+});
+
+const responseDetails = {
+    headers: {
+        "Content-Type": "text/html;charset=UTF-8",
+    },
+};
+
+const data = {
+    count: 0,
+};
+
+const handleRequest = async (request) => {
+    const url = new URL(request.url);
+    const path = url.pathname;
+    if (path === "/visits") {
+        data.count++;
+        return new Response(await renderFile("visits.eta", data), responseDetails);
+    } else if (path === "/meaning") {
+        return new Response("Seeking truths beyond meaning of life, you will find 43.");
+    } else {
+        return new Response("Nothing here yet.");
+    }
+
+};
+
+serve(handleRequest, { port:7777 });
